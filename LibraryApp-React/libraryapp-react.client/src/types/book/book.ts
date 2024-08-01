fa6174cf92ea613caa040289425a631adca5840c
Kacper <k.turek1995@gmail.com>
2024-08-01 22:16:06 +0200

    Created a new folder 'book' for type definitions in the 'types' directory: - Added Book and Books types:   - book.ts: defines the Book type, used in the bookItem and bookPage components   - books.ts: defines the Books type, used in the booksPage and booksItem components

---
+++ LibraryApp-React/libraryapp-react.client/src/types/book/book.ts
@@ -0,0 +1,16 @@
+export type KsiazkaTsx = {
+    label: string,
+    control: string,
+}
+
+export type KsiazkaChoose = {
+    label: string,
+    select: string,
+    options: string[],
+}
+
+export type KsiazkaCheck = {
+    type: string,
+    id: string,
+    label: string,
+}
